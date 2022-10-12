eca567d1fabde6ec1854cb5ec5e71ae9bc46d899
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-10-11 21:38:19 -0400

    add score component

---
+++ src/components/Score/Score.jsx
@@ -0,0 +1,11 @@
+import Student from "../Student/Student";
+
+const Score = (props) => {
+  return ( 
+    <>
+        <h4>{props.score.scores}</h4>     
+    </>
+  );
+}
+
+export default Score;
--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -1,8 +1,11 @@
+import Score from "../Score/Score";
+
 const Student = (props) => {
   return ( 
     <>
       <h2>{props.student.name}</h2>
       <h4>{props.student.bio}</h4>
+      < Score score={props.student.scores}/>
     </>
   );
 }
